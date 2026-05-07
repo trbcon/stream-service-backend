700a27595aa13d8a0b3deccb496c433c84b6bab1
trbcon <pahax.off@gmail.com>
2026-05-07 18:34:17 +0300

    feat: was added login input file

---
+++ src/modules/auth/sessions/inputs/login.input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+@InputType()
+export class LoginUserInput {
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  public login: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @Length(8, 64)
+  public password: string;
+}
