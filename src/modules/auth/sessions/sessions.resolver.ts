1b5b353d44e6c62afa530efef6d3475f70b393b1
trbcon <pahax.off@gmail.com>
2026-05-07 19:59:48 +0300

    feat: logout and login sessoin resolver was written

--- src/modules/auth/sessions/sessions.resolver.ts
+++ src/modules/auth/sessions/sessions.resolver.ts
@@ -1,7 +1,26 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { SessionsService } from './sessions.service';
+import { UserModel } from '../account/models/user.model';
+import type { GqlContext } from 'src/shared/types/gql-context.types';
+import { loginInput } from './inputs/login.input';
 
 @Resolver('Session')
 export class SessionsResolver {
-  constructor(private readonly sessionsService: SessionsService) {}
+  public constructor(private readonly sessionsService: SessionsService) {}
+
+  @Mutation(() => UserModel, { name: 'login' })
+  public async login(
+    @Context() { req }: GqlContext,
+    @Args('data') input: loginInput,
+  ) {
+    return this.sessionsService.login(req, input);
+  }
+
+  @Mutation(() => Boolean, { name: 'logout' })
+  public async logout(
+    @Context() { req }: GqlContext,
+    @Args('data') input: loginInput,
+  ) {
+    return this.sessionsService.logout(req);
+  }
 }
