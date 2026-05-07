d4d38af5ac3d61c0b0e933821b340bd6fdd298c5
trbcon <pahax.off@gmail.com>
2026-05-07 18:31:15 +0300

    feat: was added func with names 'login' and 'logout'

--- src/modules/auth/sessions/sessions.service.ts
+++ src/modules/auth/sessions/sessions.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
-export class SessionsService {}
+export class SessionsService {
+  public constructor(private readonly prismaService: PrismaService) {}
+
+  public async login() {}
+
+  public async logout() {}
+}
